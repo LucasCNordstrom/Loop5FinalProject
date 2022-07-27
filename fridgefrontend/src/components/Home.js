ff90d4c861a1938666f6fa0c21fd81980608517c
Laura-Foxey <roura_sasunaru@hotmail.com>
2022-07-27 15:04:04 +0200

    add heigh to pages

--- fridgefrontend/src/components/Home.js
+++ fridgefrontend/src/components/Home.js
@@ -2,10 +2,10 @@ import '../CSS/Home.css';
 
 function Home() {
   return (
-    <>
+    <div className='home'>
       <img className="logo-img" src="https://i.imgur.com/skhQcVC.png" />
         HELLO THIS IS HOMEPAGE WELCOME TO MY WEBSITE TYVM FOR VISITING YOU ARE AMAZING GG
-    </>
+    </div>
   );
 }
 
