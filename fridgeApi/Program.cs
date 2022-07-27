31b2eafa420c8d9f016ca7c8a5a6cc0f891e155d
lucepuce <lucas_keno@hotmail.com>
2022-07-27 14:45:52 +0200

    added site in cors

--- fridgeApi/Program.cs
+++ fridgeApi/Program.cs
@@ -20,7 +20,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://fridgeapp-45c8b.web.app", "http://localhost:3000", "https://lucepuce.github.io"));
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins(
+    "https://fridgeapp-45c8b.web.app",
+    "http://localhost:3000", 
+    "https://lucepuce.github.io",
+    "https://purple-grass-03d55a510.1.azurestaticapps.net/"));
 
 //TODO not any method and this will be changed later I HOPE
 
